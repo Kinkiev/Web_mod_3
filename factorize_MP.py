6a9395df3daa7c6aa2eb331e14ba0876984f5b33
Kinkiev <127745197+Kinkiev@users.noreply.github.com>
2023-09-03 23:27:03 +0300

    factor2

--- factorize_MP.py
+++ factorize_MP.py
@@ -7,7 +7,6 @@ def factorize_worker(num, result_dict):
         if num % i == 0:
             factors.append(i)
     result_dict[num] = factors if factors else [num]
-    # print (result_dict)
 
 def factorize_parallel(numbers):
     manager = multiprocessing.Manager()
@@ -25,7 +24,7 @@ def factorize_parallel(numbers):
     return [result_dict[num] for num in numbers]
 
 def time_do():
-    numbers = [128, 255, 99990, 10651060]
+    numbers = [128, 255, 99999, 10651060]
     start_time = time.time()
     factors_list = factorize_parallel(numbers)
     end_time = time.time()
@@ -35,15 +34,18 @@ def time_do():
 if __name__ == '__main__':
     factors_list, execution_time = time_do()
 
-    # print(f"Factors: {factors_list}")
-    print(f"Execution Time: {execution_time} seconds")
+    print(f"Factors for 128: {factors_list[0]} \n")
+    print(f"Factors for 256: {factors_list[1]} \n")
+    print(f"Factors for 99999: {factors_list[2]} \n")
+    print(f"Factors for 10651060: {factors_list[3]} \n")
+    print(f"Execution Time: {execution_time} seconds \n")
     
-    a = factorize_parallel(128, 255, 99999, 10651060)
-    b = factorize_parallel(128, 255, 99999, 10651060)
-    a, b, c, d  = factorize_parallel(128, 255, 99999, 10651060)
-    a, b, c, d  = factorize_parallel(128, 255, 99999, 10651060)
+    a = [1, 2, 4, 8, 16, 32, 64, 128]
+    b = [1, 3, 5, 15, 17, 51, 85, 255]
+    c = [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
+    d = [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
     
-    assert a == [1, 2, 4, 8, 16, 32, 64, 128]
-    assert b == [1, 3, 5, 15, 17, 51, 85, 255]
-    assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
-    assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
+    assert a == factors_list[0]
+    assert b == factors_list[1]
+    assert c == factors_list[2]
+    assert d == factors_list[3]
